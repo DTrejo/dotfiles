6475d33db89fb61fe08a1f00b5cfd3990492bb39
David Trejo <david.daniel.trejo@gmail.com>
2011-08-01 01:20:10 -0700

    install.sh moves and renames "old" dotfiles

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ BASE=$(readlink -f $(pwd))
 for x in $BASE/dot-* ; do
   fname=$(basename $x)
   realdot=$(echo $fname | sed -e 's/^dot-/./')
+
+  if [ -f ~/$realdot ]; then
+    echo Moving ~/$realdot to ~/$realdot.old`date +%m%d%H%M%Y.%S`
+    mv $HOME/$realdot $HOME/$realdot.old
+  fi
+
   echo Linking ~/$realdot to $x
   ln -s $x $HOME/$realdot
+
 done
