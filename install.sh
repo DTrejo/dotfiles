9cf696b4a4798002d576cb9ef55b512799253e2d
DTrejo <56119+DTrejo@users.noreply.github.com>
2022-07-26 17:49:32 -0700

    install.sh: ~/.zshrc sources self

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for x in $BASE/dot-* ; do
   realdot=$(echo $fname | sed -e 's/^dot-/./')
 
   if [ -f ~/$realdot ]; then
-    echo Moving ~/$realdot to ~/$realdot.old`date +%m%d%H%M%Y.%S`
+    echo Moving ~/$realdot to ~/$realdot.old$(date +%m%d%H%M%Y.%S)
     mv $HOME/$realdot $HOME/$realdot.old
   fi
 
@@ -19,5 +19,10 @@ done
 echo Also Linking ~/bin to $BASE/bin
 ln -s $BASE/bin $HOME/bin
 
-echo "touch ~/.private, a place to put secrets"
-touch ~/.private
+echo "create ~/.private, a place to put secrets"
+touch $HOME/.private
+
+echo "Editing ~/.zshrc so it sources ~/.profile"
+echo "" >> $HOME/.zshrc
+echo "# use github.com/dtrejo/dotfiles" >> $HOME/.zshrc
+echo 'source $HOME/.profile' >> $HOME/.zshrc
